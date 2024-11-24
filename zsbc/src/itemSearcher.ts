4138dbdbf8f540c4065f88042ae1e0487650e0da
blue-beaker <yyy5566778899@126.com>
2024-11-24 22:51:25 +0800

    add missing semicolons

--- zsbc/src/itemSearcher.ts
+++ zsbc/src/itemSearcher.ts
@@ -4,12 +4,12 @@ var registries:vscode.QuickPickItem[] = [];
 export async function setRegistries(registryMap:Map<string,string>){
     registries=[];
     registryMap.forEach((value,key,map)=>{
-        registries.push({label:value,description:key})
+        registries.push({label:value,description:key});
     });
 }
 export async function showItemSearcher(){
-    var result = await vscode.window.showQuickPick(registries,{title:"Item Searcher",placeHolder:"Pick an item to insert",canPickMany:false})
+    var result = await vscode.window.showQuickPick(registries,{title:"Item Searcher",placeHolder:"Pick an item to insert",canPickMany:false});
     if(!result || !result.description){return;}
-    var editor = vscode.window.activeTextEditor
+    var editor = vscode.window.activeTextEditor;
     editor?.insertSnippet(new vscode.SnippetString(result.description));
 }
